fdece7d202b2a2c5fe3df264477dcfcd60044552
Alejandro Mc Ewen <mcewenalejandro@gmail.com>
2023-02-26 17:01:55 -0500

    Adding template and localization

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+Route::get('/language/{locale}', 'App\Http\Controllers\HomeController@locale')->name('home.language');
