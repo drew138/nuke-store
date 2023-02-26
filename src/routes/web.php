4e1f4b4b94a39e893b850c120a8287b5d54da0dc
Juan Manuel <jmmunoza@eafit.edu.co>
2023-02-26 17:08:12 -0500

    web routes to bomb

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Bombs Controller routes
+Route::get('   /bombs',        'App\Http\Controllers\BombController@index')->name('bomb.index');
+Route::get('   /bombs/create', 'App\Http\Controllers\BombController@create')->name('bomb.create');
+Route::post('  /bombs/save',   'App\Http\Controllers\BombController@save')->name('bomb.save');
+Route::get('   /bombs/{id}',   'App\Http\Controllers\BombController@show')->name('bomb.show');
+Route::delete('/bombs/destroy','App\Http\Controllers\BombController@destroy')->name('bomb.destroy');
