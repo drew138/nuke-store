2b1b129d28d2b02ddb375456ada9a26fb01d20c9
drew <58788781+drew138@users.noreply.github.com>
2023-03-15 11:13:09 -0500

    created admin and user controllers for the bomb, review, order and home views

---
+++ src/app/Http/Controllers/admin/BombController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Interfaces\ImageStorage;
+use App\Models\Bomb;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class BombController extends Controller
+{
+    public function index(string $query): View
+    {
+        $data = [];
+        $data['bombs'] = Bomb::searchByName($query);
+        return view('admin.bombs.index')->with('data', $data);
+    }
+
+    public function show(string $id): View|RedirectResponse
+    {
+        if (filter_var($id, FILTER_VALIDATE_INT) == true) {
+
+            $bomb = Bomb::findOrFail($id);
+
+            $data = [];
+            $data['bomb'] = $bomb;
+
+            return view('admin.bombs.show')->with('data', $data);
+        }
+
+        return redirect()->route('admin.home.index');
+    }
+
+    public function create(): View
+    {
+        return view('admin.bombs.create');
+    }
+
+    public function save(Request $request): RedirectResponse
+    {
+        Bomb::validate($request);
+
+        # Storing the bomb image and getting its path
+        $storeInterface = app(ImageStorage::class);
+        $image_url = $storeInterface->store($request);
+
+        Bomb::create([
+            'name' =>                  $request['name'],
+            'type' =>                  $request['type'],
+            'price' =>                 $request['price'],
+            'location_country' =>      $request['location_country'],
+            'manufacturing_country' => $request['manufacturing_country'],
+            'stock' =>                 $request['stock'],
+            'destruction_power' =>     $request['destruction_power'],
+            'image' =>                 $image_url
+        ]);
+
+        return back()->withSuccess(__('bomb.successfully'));
+    }
+
+    public function destroy(Request $request): View|RedirectResponse
+    {
+        Bomb::destroy($request->only(['id']));
+
+        return redirect()->route('admin.bombs.index');
+    }
+}
