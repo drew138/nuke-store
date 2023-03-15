a081a11a08c88260682cad712c4bc9e2cbc781fe
Juan Manuel <jmmunoza@eafit.edu.co>
2023-03-15 15:51:15 -0500

    Admin bombs table advances

--- src/resources/views/admin/bombs/index.blade.php
+++ src/resources/views/admin/bombs/index.blade.php
@@ -58,56 +58,53 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr class="border-b dark:border-gray-700">
-                                    <th scope="row"
-                                        class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">27
-                                    </th>
-                                    <td class="px-4 py-3">
-                                        <img class="w-10 h-10 object-cover rounded"
-                                            src="https://www.eltiempo.com/uploads/2022/03/07/62265a8e50a57.jpeg">
-                                    </td>
-                                    <th scope="row"
-                                        class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">La
-                                        destruye planetas</th>
-                                    <td class="px-4 py-3">Hydrogen bomb</td>
-                                    <td class="px-4 py-3">Colombia</td>
-                                    <td class="px-4 py-3">Spain</td>
-                                    <td class="px-4 py-3">$25000000</td>
-                                    <td class="px-4 py-3">24</td>
-                                    <td class="px-4 py-3">175000 Mt</td>
-                                    <td class="px-4 py-3">25 nov 2022</td>
-                                    <td class="px-4 py-3 justify-end">
-                                        <button id="apple-imac-27-dropdown-button"
-                                            data-dropdown-toggle="apple-imac-27-dropdown"
-                                            class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
-                                            type="button">
-                                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path
-                                                    d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                                            </svg>
-                                        </button>
-                                        <div id="apple-imac-27-dropdown"
-                                            class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-                                            <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
-                                                aria-labelledby="apple-imac-27-dropdown-button">
-                                                <li>
+                                @foreach ($data['bombs'] as $bomb)
+                                    <tr class="border-b dark:border-gray-700">
+                                        <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $bomb->getId() }}</th>
+                                        <td class="px-4 py-3">
+                                            <img class="w-10 h-10 object-cover rounded"
+                                                src="{{ URL::asset($bomb->getImage()) }}">
+                                        </td>
+                                        <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $bomb->getName() }}</th>
+                                        <td class="px-4 py-3">{{ __('bomb.' . $bomb->getType()) }}</td>
+                                        <td class="px-4 py-3">{{ __('countries.' . $bomb->getManufacturingCountry()) }}</td>
+                                        <td class="px-4 py-3">{{ __('countries.' . $bomb->getLocationCountry()) }}</td>
+                                        <td class="px-4 py-3">${{ $bomb->getPrice() }}</td>
+                                        <td class="px-4 py-3">{{ $bomb->getStock() }}</td>
+                                        <td class="px-4 py-3">{{ $bomb->getDestructionPower() }} Mt</td>
+                                        <td class="px-4 py-3">FECHA</td>
+                                        <td class="px-4 py-3 justify-end">
+                                            <button id="apple-imac-27-dropdown-button"
+                                                data-dropdown-toggle="apple-imac-27-dropdown"
+                                                class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
+                                                type="button">
+                                                <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
+                                                    viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                    <path
+                                                        d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                                                </svg>
+                                            </button>
+                                            <div id="apple-imac-27-dropdown"
+                                                class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                                                <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
+                                                    aria-labelledby="apple-imac-27-dropdown-button">
+                                                    <li>
+                                                        <a href="#"
+                                                            class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Show</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#"
+                                                            class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+                                                    </li>
+                                                </ul>
+                                                <div class="py-1">
                                                     <a href="#"
-                                                        class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Show</a>
-                                                </li>
-                                                <li>
-                                                    <a href="#"
-                                                        class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
-                                                </li>
-                                            </ul>
-                                            <div class="py-1">
-                                                <a href="#"
-                                                    class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
+                                                        class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
-                                </tr>
-
+                                        </td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
