e5b610964e7cece233adc7d26434a18b31bd3a70
Juan Manuel <jmmunoza@eafit.edu.co>
2023-03-17 09:26:03 -0500

    Bug count null in app.layout solved

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -35,10 +35,13 @@
                                 d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z">
                             </path>
                         </svg>
-                        <span
-                            class="inline-flex items-center justify-center w-4 h-4 ml-1 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
-                            {{ count(session()->get('shopping_cart')) }}
-                        </span>
+                        @if (is_null(session()->get('shopping_cart')))
+                            <span
+                                class="inline-flex items-center justify-center w-4 h-4 ml-1 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">0</span>
+                        @else
+                            <span
+                                class="inline-flex items-center justify-center w-4 h-4 ml-1 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">{{ count(session()->get('shopping_cart')) }}</span>
+                        @endif
                     </a>
 
                     <form class="w-full relative hidden md:block" action="{{ route('bombs.search') }}" method="POST">
