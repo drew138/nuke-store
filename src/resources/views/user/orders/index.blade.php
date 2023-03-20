06db5a3450f12035a6353622595a84b5c119de00
Mirola <jmmunoza@eafit.edu.co>
2023-03-20 13:31:41 -0500

    Order completed alert added

--- src/resources/views/user/orders/index.blade.php
+++ src/resources/views/user/orders/index.blade.php
@@ -3,7 +3,22 @@
 @section('content')
     <div class="p-4">
         <section class="bg-gray-50 mb-24 dark:bg-gray-900 p-3 sm:p-5">
+
             <div class="mx-auto max-w-7xl px-2 lg:px-4">
+                @if (session('success'))
+                    <div class="w-full mb-4 border border-green-600 flex p-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+                        role="alert">
+                        <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor"
+                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                        <div>
+                            <span class="font-medium">{{ session('success') }}
+                        </div>
+                    </div>
+                @endif
                 <h1 class="mb-12 text-4xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span
                         class="text-transparent bg-clip-text bg-gradient-to-r to-blue-600 from-sky-400">{{ __('orders.your_orders') }}</span>
                 </h1>
