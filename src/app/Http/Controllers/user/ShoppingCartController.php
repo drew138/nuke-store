d2be004fec5a9d57cabc13ca7a225fb9ac7cbf36
Mirola <jmmunoza@eafit.edu.co>
2023-03-20 19:27:14 -0500

    Buggatti Shopping Cart

--- src/app/Http/Controllers/user/ShoppingCartController.php
+++ src/app/Http/Controllers/user/ShoppingCartController.php
@@ -14,7 +14,10 @@ class ShoppingCartController extends Controller
 {
     public function index(): View
     {
+        
         $cartData = session()->get('shopping_cart');
+        if(is_null($cartData)) $cartData = [];
+       
         $bombs = Bomb::findMany(array_keys($cartData));
 
         $data = [];
