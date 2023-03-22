5c4fdf7ed9816d31b190f2fd37c3403b1915f54c
Alejandro Mc Ewen <mcewenalejandro@gmail.com>
2023-03-22 09:35:27 -0500

    Review changes

--- src/app/Interfaces/PaymentService.php
+++ src/app/Interfaces/PaymentService.php
@@ -2,9 +2,9 @@
 
 namespace App\Interfaces;
 
-use Illuminate\Http\Request;
+use App\Models\User;
 
 interface PaymentService
 {
-    public function pay(Request $request): int;
+    public function pay(User $user, int $total): int;
 }
