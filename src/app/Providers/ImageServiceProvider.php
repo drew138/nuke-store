5f61be0ed7b2ab60f6fddd60ab7ad3c2fcb858df
drew <58788781+drew138@users.noreply.github.com>
2023-05-06 18:07:44 -0500

    changes

--- src/app/Providers/ImageServiceProvider.php
+++ src/app/Providers/ImageServiceProvider.php
@@ -2,16 +2,23 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\ServiceProvider;
 use App\Interfaces\ImageStorage;
 use App\Util\ImageLocalStorage;
-use Illuminate\Support\ServiceProvider;
+use App\Util\ImageGCPStorage;
 
 class ImageServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->bind(ImageStorage::class, function () {
-            return new ImageLocalStorage();
+        $this->app->bind(ImageStorage::class, function ($app, $params) {
+            // dd($params);
+            // $storage = $params['storage'];
+            // if ($storage == "local") {
+            //     return new ImageLocalStorage();
+            // } else if ($storage == "gcp") {
+            return new ImageGCPStorage();
+            // }
         });
     }
 }
--- src/config/filesystems.php
+++ src/config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -56,15 +56,24 @@ return [
             'throw' => false,
         ],
 
-        'gcs' => [
-            'driver' => 'gcs',
-            'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'your-project-id'),
-            'key_file' => env('GOOGLE_CLOUD_KEY_FILE', base_path('service-account.json')), // optional: /path/to/service-account.json
-            'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'your-bucket'),
-            'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
-            'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
-            'visibility' => 'public',
+        // 'gcs' => [
+        //     'driver' => 'gcs',
+        //     'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'your-project-id'),
+        //     'key_file' => env('GOOGLE_CLOUD_KEY_FILE', base_path('service-account.json')), // optional: /path/to/service-account.json
+        //     'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'your-bucket'),
+        //     'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
+        //     'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
+        //     'visibility' => 'public',
+        // ],
+
+        'gcp' => [
+            'driver' => 'gcp',
+            'project_id' => env('GOOGLE_CLOUD_PROJECT_ID'),
+            'key_file' => env('SERVICE_ACCOUNT_PATH'),
+            'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET'),
         ],
+
+
 
     ],
 
