9e1bf3358c67935dad56b8276301a7e776c30a4a
Drew138 <Drew138@users.noreply.github.com>
2023-05-10 12:42:12 +0000

    PHP Linting (Pint)

--- src/app/Http/Controllers/User/RickAndMortyController.php
+++ src/app/Http/Controllers/User/RickAndMortyController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Http;
 use Illuminate\View\View;
-use App\Http\Controllers\Controller;
 
 class RickAndMortyController extends Controller
 {
@@ -14,6 +14,7 @@ class RickAndMortyController extends Controller
         $url = 'https://rickandmortyapi.com/api/character';
         $results = Http::get($url)['results'];
         $data = ['characters' => $results];
+
         return view('user.rickAndMorty.index')->with('data', $data);
     }
 }
